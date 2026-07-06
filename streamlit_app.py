c3617c3646ea03d67584787ecdd122ce2cc35a9a
Zhuoan <zhouzhuoan@gmail.com>
2026-07-06 13:08:34 -0400

    fix: increase Speak button height to 130px to align with form row

--- streamlit_app.py
+++ streamlit_app.py
@@ -1039,7 +1039,7 @@ div[data-testid="stFormSubmitButton"] button[kind="primaryFormSubmit"] {
     height: 100px !important;
 }
 [data-testid="stColumn"] iframe {
-    height: 100px !important; min-height: 100px !important;
+    height: 130px !important; min-height: 130px !important;
     width: 100% !important;
 }
 /* Multiple Choice Options toggle button */
